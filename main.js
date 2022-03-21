db589ceaccff9ed2ad5a404645d76da0b6d86162
Balázs <kelecsenyibalazs99@gmail.com>
2022-03-21 14:16:44 +0100

    new menu

---
+++ main.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () =>{
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+window.addEventListener('scroll', reveal);
+    function reveal(){
+  
+        var revealleft = document.querySelectorAll('.revealleft');
+        var revealright = document.querySelectorAll('.revealright');
+        var windowheight = window.innerHeight;
+        var revealtop = revealleft[0].getBoundingClientRect().top;
+        var revealpoint = 150;
+  
+        if(revealtop < windowheight - revealpoint){
+            revealleft[0].classList.add('active');
+            revealright[0].classList.add('active');
+        }
+        else{
+            revealleft[0].classList.remove('active');
+            revealright[0].classList.remove('active');
+        }
+    };
