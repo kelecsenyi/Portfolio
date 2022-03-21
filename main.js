19a2b415c2d568297e1099c1e8d89047ef9a5b28
Balázs <kelecsenyibalazs99@gmail.com>
2022-03-21 14:42:22 +0100

    hide menu

--- main.js
+++ main.js
@@ -4,9 +4,18 @@ menuBtn.addEventListener('click', () =>{
     if (!menuOpen) {
         menuBtn.classList.add('open');
         menuOpen = true;
-    } else {
+    }  else  {
         menuBtn.classList.remove('open');
         menuOpen = false;
+    }
+
+    window.onscroll = function() {myFunction()};
+
+    function myFunction() {
+      if (document.documentElement.scrollTop > 100) {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+      } 
     }
 });
 
